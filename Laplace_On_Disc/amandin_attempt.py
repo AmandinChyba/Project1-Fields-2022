f8afe366efbe6f524f1e7c0b7f615a8db3e955ae
AmandinChyba <achybarabeendran@mines.edu>
2022-07-15 11:55:25 -0400

    fixed the acceptance matrix with the help of Quinn

--- Laplace_On_Disc/amandin_attempt.py
+++ Laplace_On_Disc/amandin_attempt.py
@@ -1,10 +1,11 @@
 import jax.random as random
 import jax.numpy as jnp
+import jax
 import numpy as np
 import time
 from tqdm import tqdm
 
-
+print(jax.default_backend())
 def solve_at(g, k, r, theta, t, key):
  
   keys = random.split(key, 2);
@@ -12,8 +13,8 @@ def solve_at(g, k, r, theta, t, key):
   x = random.uniform(keys[0], shape=(1,t)) * 2 * np.pi
   f = k(r, theta - x)
   
-  A1 = jnp.tile(f, (t,1))
-  A2 = jnp.tile(jnp.transpose(1/f), (1,t))
+  A1 = jnp.tile(jnp.transpose(f), (1,t))
+  A2 = jnp.tile(1/f, (t,1))
   
   A = jnp.minimum(1, jnp.tril(jnp.multiply(A1, A2)))
   diag_elements = jnp.diag_indices_from(A)
@@ -27,30 +28,16 @@ def solve_at(g, k, r, theta, t, key):
     
   col = 0
   y = []
+  y = np.empty(t)
   for i in tqdm(range(t)):
       if check[i+1,col] > 0:
-          y.append(x[1,col])
+          #y.append(x[1,col])
+          y[i] = x[1,col]
           col += 1
       else:
-          y.append(x[1,col])
-  '''
-  # should be faster but it is not
-  row = 0
-  y = []
-  col = 0
-  while(row < t and col < t):
-    # find how many times a proposal x would be rejected
-    rowChange = jnp.where(check[row:,col] > 0)[0]
-    if (not rowChange.any()):
-      rowChange = t - row
-    else:
-      rowChange = rowChange[0] + 1
-
-    # update stuff
-    row = rowChange + row
-    y += [x[1,col]]*rowChange
-    col += 1
-  '''
+          #y.append(x[1,col])
+          y[i] = x[1,col]
+  
   #print(jnp.asarray(y))
   #print(g(jnp.asarray(y)))
   return y, jnp.mean(g(jnp.asarray(y)))
@@ -61,7 +48,7 @@ def poisson_kernel(r, theta):
           (1 - 2 * r * jnp.cos(theta) + jnp.power(r, 2)))
 
 t = 1000
-r = 0.5
+r = 0.9
 theta = np.pi / 2
 key = random.PRNGKey(0)
 samples, ans = solve_at(jnp.sin, poisson_kernel, r, theta, t, key)
