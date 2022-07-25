43099ecff1193ef5b5f107858a52cd932d3bce25
AmandinChyba <achybarabeendran@mines.edu>
2022-07-25 09:35:51 -0400

    unpushed updated code

--- Laplace_On_Disc/amandin_attempt.py
+++ Laplace_On_Disc/amandin_attempt.py
@@ -16,39 +16,27 @@ def solve_at(g, k, r, theta, t, key):
   A1 = jnp.tile(jnp.transpose(f), (1,t))
   A2 = jnp.tile(1/f, (t,1))
   
-  A = jnp.minimum(1, jnp.tril(jnp.multiply(A1, A2)))
-  diag_elements = jnp.diag_indices_from(A)
-  A = A.at[diag_elements].set(0)
-
-  u = jnp.tril(random.uniform(keys[1], shape=(t,t)))
-  diag_elements = jnp.diag_indices_from(u)
-  u = u.at[diag_elements].set(0)
-  
+  A = jnp.minimum(1, jnp.tril(jnp.multiply(A1, A2), k=-1))
+  u = jnp.tril(random.uniform(keys[1], shape=(t,t)), k=-1)
   check = jnp.subtract(A, u)
     
   col = 0
-  y = []
   y = np.empty(t)
   for i in tqdm(range(t)):
       if check[i+1,col] > 0:
-          #y.append(x[1,col])
           y[i] = x[1,col]
           col += 1
       else:
-          #y.append(x[1,col])
           y[i] = x[1,col]
   
-  #print(jnp.asarray(y))
-  #print(g(jnp.asarray(y)))
   return y, jnp.mean(g(jnp.asarray(y)))
   
 
 def poisson_kernel(r, theta):
-  return ((1 - jnp.power(r, 2)) / 
-          (1 - 2 * r * jnp.cos(theta) + jnp.power(r, 2)))
+  return (1 - jnp.power(r, 2))/(1 - 2 * r * jnp.cos(theta) + jnp.power(r, 2))
 
 t = 1000
-r = 0.9
+r = 0.5
 theta = np.pi / 2
 key = random.PRNGKey(0)
 samples, ans = solve_at(jnp.sin, poisson_kernel, r, theta, t, key)
